630e12ba8c9fb96bac3f06b3cf9bfc0b11e4ebfd
r1nk1337 <mar4ikmarton@gmail.com>
2024-07-28 15:10:52 +0300

    v 1.5
    added cool things)

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,9 +3,27 @@
     const btn = document.querySelector('#submit');
     const container = document.querySelector('.for-img');
     const htwo = document.querySelector('#yourName');
+    const heading = document.querySelector('#heading');
+    const paragraph = document.querySelector('#paragraph');
     const modalBox = document.querySelector('.modal-box');
     const btnProceed = document.querySelector('.btn-proceed');
     const btntrouble = document.querySelector('#trouble');
+    const forInp = document.querySelector('.for-inputs');
+    const select = document.getElementById('language');
+    const eng = document.querySelector('.en');
+    const ukr = document.querySelector('.ua');
+    const translations = {
+        en: {
+            heading: 'Hi there!👋',
+            paragraph: 'On this site you can see a random avatar generator powered by <span class="Db">DiceBear</span>. You must just simply entеr your name or nickname or what ever you want just enjoy. Good luck from <span class="dd"> Dream Developement</span>',
+            button:'Proceed',
+        },
+        ua: {
+            heading: 'Привіт!👋',
+            paragraph: 'На цьому сайті ви можете створити собі випадковий аватар з <span class="Db">DiceBear</span>. Просто введіть своє імя, або будь що. Удачі від <span class="dd"> Dream Developement</span>',
+            button: 'Поїхали',
+        },
+    };
     //const contForhtwo = document.querySelector('.for-name');
     function randomNum(min, max){
         min= Math.ceil(min);
@@ -29,9 +47,17 @@
     };
     btnProceed.addEventListener('click', () => {
         modalBox.classList.replace('open', 'closed');
+        forInp.classList.toggle('closed');
     });
     btntrouble.addEventListener('click', () => {
         location.reload();
+    });
+    select.addEventListener('change', (event) => {
+        const selected = event.target.value;
+        heading.innerHTML = translations[selected].heading;
+        paragraph.innerHTML = translations[selected].paragraph;
+        btnProceed.innerHTML = translations[selected].button;
+
     });
 //0096c7`<img src="https://api.dicebear.com/6.x/open-peeps/svg?seed=${nameValue}&backgroundColor=0096c7&radius=10" alt="thx dicebear">`;  
 
